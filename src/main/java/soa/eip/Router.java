ab7764e037542a6937f1c8dfa3b163500b6d7f27
eduardoRuizC <764539@unizar.es>
2020-12-14 17:16:40 +0100

    Add the max functionality

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -3,6 +3,9 @@ package soa.eip;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class Router extends RouteBuilder {
 
@@ -11,7 +14,16 @@ public class Router extends RouteBuilder {
   @Override
   public void configure() {
     from(DIRECT_URI)
-      .log("Body contains \"${body}\"")
+      .log("Body contains \"${body}\"").process(exchange -> {
+        String[] consult = exchange.getIn().getBody(String.class).split("max:");
+        if (consult.length > 1 ) {
+          String newConsult = consult[0];
+          newConsult += "?count=" + consult[1];
+          exchange.getOut().setBody(newConsult);
+        } else {
+          exchange.getOut().setBody(consult[0]);
+        }
+      })
       .log("Searching twitter for \"${body}\"!")
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
